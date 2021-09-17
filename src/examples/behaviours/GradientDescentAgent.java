453dfac70a545536f1709943b43329ff370766b6
Carlos Ch <crch98@protonmail.com>
2021-09-17 03:02:40 -0500

    Implemented class for gradient descent algorithm

--- src/examples/behaviours/GradientDescentAgent.java
+++ src/examples/behaviours/GradientDescentAgent.java
@@ -1,5 +1,3 @@
-package hmw;
-
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 
@@ -8,6 +6,68 @@ public class GradientDescentAgent extends Agent {
 	protected void setup() {
 		System.out.println("Agent " + getLocalName() + " started.");
 		addBehaviour(new GDOneShotBehaviour());
+	}
+
+	/**
+	 * Implements Gradient Descent algorithm for function f(x) = x^2
+	 * with bounds [-1, 1].
+	 *
+     */
+	private class GradientDescent {
+		private double stepSize;
+		private int nIter;
+
+		public GradientDescent(double stepSize, int nIter) {
+			this.stepSize = stepSize;
+			this.nIter = nIter;
+		}
+
+		/**
+		 * double -> double
+		 *
+		 * produces the square of parameter x
+		 * this is f(x) = x^2
+		 *
+		 */
+		private double objective(double x) {
+			return x * x;
+		}
+
+		/**
+  		 * double -> double
+		 *
+		 * produces the result of multiplying x by 2
+		 * this is the derivative of f(x)
+		 * f'(x) = 2x
+		 *
+		 */
+		private double derivative(double x) {
+			return 2 * x;
+		}
+
+		/**
+		 * shows the result obtained from each iteration of the
+		 * gradient descent algorithm
+		 *
+		 */
+		public void calculate() {
+			double leftBound = -1.0;
+			double rightBound = 1.0;
+
+			// initialize to a random number in the range [-1, 1]
+			double solution = leftBound + (Math.random() *
+				(rightBound - leftBound));
+
+			for (int i = 0; i < this.nIter; i++) {
+				var gradient = derivative(solution);
+				// update solution value
+				solution = solution - (gradient * this.stepSize);
+				// show the new solution
+				System.out.printf(">%d f(%.5f) = %.5f\n", i, solution,
+					objective(solution));
+			}
+		}
+
 	}
 
 	private class GDOneShotBehaviour extends OneShotBehaviour {
